97762f9dbe6e5504aa1e246ab5d6514f9b784981
Dmitrii <webstorybegin@gmail.com>
2022-01-27 20:17:32 +0200

    test

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-import { StartPage } from "components/StartPage/StartPage";
+import { useState } from 'react';
+import { StartPage } from 'components/StartPage/StartPage';
 
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from '@material-ui/styles';
 import { Quiz } from 'components/Quiz/Quiz';
 
 const useStyles = makeStyles({
   root: {
-    height: "100vh",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundImage:
-      "-webkit-linear-gradient(-61deg, #ffffff 50%, #FFF3EB 50%)",
-    fontFamily: "Inter",
+    height: '100vh',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundImage: '-webkit-linear-gradient(-61deg, #ffffff 50%, #FFF3EB 50%)',
+    fontFamily: 'Inter',
   },
 });
 
@@ -24,11 +23,7 @@ const Game = () => {
 
   return (
     <div className={classes.root}>
-      {start ? (
-        <Quiz start={start} setStart={setStart} />
-      ) : (
-        <StartPage setStart={setStart} />
-      )}
+      {start ? <Quiz start={start} setStart={setStart} /> : <StartPage setStart={setStart} />}
     </div>
   );
 };
